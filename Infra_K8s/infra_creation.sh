9ac6b2be9aa79b86b2557e5c3234e35af1fedb8c
simplon-lerouxDunvael <simplon.lerouxdunvael@gmail.com>
2023-02-28 15:06:58 +0100

    code added

--- Infra_K8s/infra_creation.sh
+++ Infra_K8s/infra_creation.sh
@@ -28,8 +28,8 @@ echo "AKS cluster credentials retrieved."
 
 # Create Prod & Qua namespaces
 echo "Creating Prod & Qua namespaces for QAL & Public deploy"
-kubectl create namespace prod
 kubectl create namespace qua
+kubectl create namespace prod
 echo "Namespaces created"
 
 # Create Redis database secret
@@ -40,11 +40,6 @@ echo "Creating Redis database secret for namespace prod..."
 kubectl create secret generic redis-secret-duna --from-literal=username=$redusrprod --from-literal=password=$redpassprod -n prod
 echo "Redis database secret created."
 
-# Add Jetstack Helm repository
-echo "Adding Jetstack Helm repository..."
-helm repo add jetstack https://charts.jetstack.io
-echo "Jetstack Helm repository added."
-
 # Install NGINX Ingress Controller
 echo "Installing NGINX Ingress Controller..."
 helm repo add nginx-stable https://helm.nginx.com/stable
@@ -52,6 +47,51 @@ helm repo update
 helm install nginx-qua nginx-stable/nginx-ingress --create-namespace -n qua --debug --set controller.ingressClass="nginx-qua"
 helm install nginx-prod nginx-stable/nginx-ingress --create-namespace -n prod --debug --set controller.ingressClass="nginx-prod"
 echo "NGINX Ingress Controller installed."
+
+# Extract External IP address
+ProdIngIP=$(kubectl get svc nginx-prod-nginx-ingress -n prod -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+QuaIngIP=$(kubectl get svc nginx-qa-nginx-ingress -n qa -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo "Prod Ingress: $ProdIngIP"
+echo "QUA Ingress: $QaIngIP"
+
+# Insert a pause in the script so that users can report IP to DNS
+read -n1 -r -p "Press Y to continue, or N to stop: " key
+
+echo
+
+if [ "$key" = 'Y' ] || [ "$key" = 'y' ]; then
+    echo "Continuing..."
+    # your code to execute if user presses Y goes here
+elif [ "$key" = 'N' ] || [ "$key" = 'n' ]; then
+    echo "Stopping..."
+    exit 1
+else
+    # do nothing
+    :
+fi
+
+# Apply applicative layers
+echo "Applying applicative configuration files..."
+kubectl apply -f azure-vote.yaml -n qua
+kubectl apply -f azure-vote.yaml -n prod
+echo "Applicative configuration files applied"
+
+# Apply Ingress layer
+echo "Applying Ingress configuration files..."
+kubectl apply -f ingress_qua1.yaml -n qua
+kubectl apply -f ingress_prod1.yaml -n prod
+echo "Ingress configuration files applied."
+
+# Apply Issuer layer
+echo "Applying Let's Encrypt Issuer configuration files..."
+kubectl apply -f issuer-qua.yaml -n qua
+kubectl apply -f issuer-prod.yaml -n prod
+echo "Let's Encrypt Issuer configuration files applied."
+
+# Add Jetstack Helm repository
+echo "Adding Jetstack Helm repository..."
+helm repo add jetstack https://charts.jetstack.io
+echo "Jetstack Helm repository added."
 
 # Install cert-manager with custom DNS settings
 echo "Installing cert-manager..."
@@ -65,17 +105,28 @@ echo "cert-manager-webhook-gandi Helm chart installed."
 
 # Create Gandi API token secret
 echo "Creating Gandi API token secret..."
-kubectl create secret generic gandi-credentials --from-literal=api-token=2fbAa8tMWHBUnNwFi5EhuRgp -n qua
-kubectl create secret generic gandi-credentials --from-literal=api-token=2fbAa8tMWHBUnNwFi5EhuRgp -n prod
+kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken
+kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken -n qa
+kubectl create secret generic gandi-credentials --from-literal=api-token=$apitoken -n prod
 echo "Gandi API token secret created."
-
-# # Recover the webhook number from the cert-manager namespace
-# kubectl get secrets -n cert-manager
 
 # Create role and rolebinding for accessing secrets
 echo "Creating role and rolebinding for accessing secrets..."
-kubectl create role access-secrets --verb=get,list,watch,update,create --resource=secrets - n qua
-kubectl create role access-secrets --verb=get,list,watch,update,create --resource=secrets - n prod
-kubectl create rolebinding --role=access-secrets default-to-secrets --serviceaccount=cert-manager:cert-manager-webhook-gandi-1677588045
+hookID=$(kubectl get pods -n cert-manager | grep "cert-manager-webhook-gandi-" | cut -d"-" -f5)
+kubectl create role access-secrets --verb=get,list,watch,update,create --resource=secrets -n qa
+kubectl create role access-secrets --verb=get,list,watch,update,create --resource=secrets -n prod
+kubectl create rolebinding --role=access-secrets default-to-secrets --serviceaccount=cert-manager:cert-manager-webhook-gandi-$hookID -n qa
+kubectl create rolebinding --role=access-secrets default-to-secrets --serviceaccount=cert-manager:cert-manager-webhook-gandi-$hookID -n prod
 echo "Role and rolebinding created."
 
+# Apply Certificate layer
+echo "Applying Certificate configuration files..."
+kubectl apply -f certif_qua.yaml -n qua
+kubectl apply -f certif_prod.yaml -n prod
+echo "Certificate configuration files applied."
+
+# Apply Ingress layer
+echo "Applying Ingress configuration files..."
+kubectl apply -f ingress_qua2.yaml -n qua
+kubectl apply -f ingress_prod2.yaml -n prod
+echo "Ingress configuration files applied."
